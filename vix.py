6b16d993f20ff42c6f43803b0e9122d1024a78aa
Souvik Das <souvik131@gmail.com>
2024-05-18 18:36:29 +0530

    [feat] auto fetch from nse

--- vix.py
+++ vix.py
@@ -9,10 +9,11 @@ import matplotlib.pyplot as plt
 import os
 from scipy.stats import norm
 import py_vollib.black.implied_volatility
+from curl_cffi import requests as req
 
 # Set the timezone to Asia/Kolkata
 kolkata = pytz.timezone('Asia/Kolkata')
-
+s = req.Session()
 
 
 
@@ -39,11 +40,9 @@ def implied_volatility(S, K, T, r, market_price,isCall=True):
 
 
 
-def getVariance(file_path,script,k,calcBasisVix=True):
-  file_arr=file_path.split(".")[0].split("-")
-  s=file_arr[3]
+def getVariance(date_string,df,script,k,calcBasisVix=True):
+  s=script
   if script==s:
-    date_string = "-".join(file_arr[len(file_arr)-3:])
     target_date = datetime.strptime(date_string, "%d-%b-%Y")
     target_date = kolkata.localize(target_date)
     today_date = datetime.now(kolkata)
@@ -68,11 +67,11 @@ def getVariance(file_path,script,k,calcBasisVix=True):
     Tmins=remaining_hours_today*60+daysInBetween*hoursInDay*60+expiryDayInMins
 
     #clean option chain
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-    df = pd.read_csv(StringIO(''.join([','.join(line.split(',')[1:]) for line in lines[1:]])))
-    df=df[["BID","ASK","STRIKE","ASK.1","BID.1"]]
-    df=df.rename(columns={"BID":"CALLBID","ASK":"CALLASK","STRIKE":"STRIKE","ASK.1":"PUTASK","BID.1":"PUTBID"})
+    # with open(file_path, 'r') as file:
+    #     lines = file.readlines()
+    # df = pd.read_csv(StringIO(''.join([','.join(line.split(',')[1:]) for line in lines[1:]])))
+    df=df[["CE.bidprice","CE.askPrice","strikePrice","PE.askPrice","PE.bidprice"]]
+    df=df.rename(columns={"CE.bidprice":"CALLBID","CE.askPrice":"CALLASK","strikePrice":"STRIKE","PE.askPrice":"PUTASK","PE.bidprice":"PUTBID"})
     df.replace('-', np.nan, inplace=True)
     df.dropna(inplace=True)
     df['CALLBID'] = df['CALLBID'].replace(',', '', regex=True).astype(float)
@@ -120,54 +119,70 @@ def getVariance(file_path,script,k,calcBasisVix=True):
     return date_string,variance,T
   return "",0,0
 
+    
+for stockname in ["NIFTY","BANKNIFTY","FINNIFTY","MIDCPNIFTY"]:
+  #fetch data and calculate variance
+  dataSet=[]
+  
 
-#fetch data and calculate variance
-dataSet=[]
-directory_path = './'
-files_in_directory = os.listdir(directory_path)
-csv_files = [file for file in files_in_directory if file.endswith('.csv')]
-for file_path in csv_files:
-  try:
-    date_string,variance,T=getVariance(file_path,"NIFTY",50,False)
-    if date_string!="":
-      dataSet.append([date_string,variance,T])
-  except Exception as e:
-    print(file_path,e)
+  url = "https://www.nseindia.com/companies-listing/corporate-filings-event-calendar"
+  r = s.get(url, impersonate="chrome")
+
+  url = "https://www.nseindia.com/api/option-chain-indices?symbol="+stockname
+  r = s.get(url, impersonate="chrome")
+  data=r.json()["records"]
+  df = pd.json_normalize(data["data"])
+
+  unique_expiry_dates = data['expiryDates']
+  for expiry in unique_expiry_dates:
+    filtered_data = df[df['expiryDate'] == expiry]
+    try:
+      diff=50
+      if stockname=='MIDCPNIFTY':
+         diff=25
+      if stockname=="BANKNIFTY":
+         diff=100
+         
+      date_string,variance,T=getVariance(expiry,filtered_data,stockname,diff,False)
+      if date_string!="":
+        dataSet.append([date_string,variance,T])
+    except Exception as e:
+      print(e)
 
 
-#plot term structure
-df=pd.DataFrame(dataSet,columns=["date","variance","time"])
-df["vol"]=np.sqrt(df["variance"]/df["time"])*100
-df=df.sort_values(by=['time'])
-dates = df["date"]
-values = df["vol"]
-fig, ax = plt.subplots()
-ax.plot(dates, values)
-plt.xticks(rotation=45) 
-plt.title('Term Structure')
-plt.xlabel('Date')
-plt.ylabel('Vol')
-plt.tight_layout() 
-plt.savefig("term.png")
+  #plot term structure
+  df=pd.DataFrame(dataSet,columns=["date","variance","time"])
+  df["vol"]=np.sqrt(df["variance"]/df["time"])*100
+  df=df.sort_values(by=['time'])
+  dates = df["date"]
+  values = df["vol"]
+  fig, ax = plt.subplots()
+  ax.plot(dates, values)
+  plt.xticks(rotation=45) 
+  plt.title('Term Structure')
+  plt.xlabel('Date')
+  plt.ylabel('Vol')
+  plt.tight_layout() 
+  plt.savefig(stockname+"_term.png")
 
-#plot fvol heatmap
-df.reset_index(inplace = True)
-ivol_matrix = pd.DataFrame(index=df['date'], columns=df['date'])
-for i in range(len(df)):
-    for j in range(len(df)):
-        if i != j:
-            var_diff = df['variance'].iloc[j] - df['variance'].iloc[i]
-            time_diff = df['time'].iloc[j] - df['time'].iloc[i]
-            if time_diff != 0: 
-                ivol = np.sqrt(abs(var_diff / time_diff)) * 100
-            else:
-                ivol = np.nan 
-            ivol_matrix.iloc[i, j] = ivol
-ivol_matrix_float = ivol_matrix.astype(float)
-plt.figure(figsize=(13, 11))
-sns.heatmap(ivol_matrix_float, annot=True, fmt=".2f", cmap="viridis", cbar_kws={'label': 'Forward Volatility'})
-plt.title('Forward Volatility Between Expiries')
-plt.xlabel('Expiry Date')
-plt.ylabel('Expiry Date')
-plt.savefig("fv.png")
-plt.show()
+  #plot fvol heatmap
+  df.reset_index(inplace = True)
+  ivol_matrix = pd.DataFrame(index=df['date'], columns=df['date'])
+  for i in range(len(df)):
+      for j in range(len(df)):
+          if i != j:
+              var_diff = df['variance'].iloc[j] - df['variance'].iloc[i]
+              time_diff = df['time'].iloc[j] - df['time'].iloc[i]
+              if time_diff != 0: 
+                  ivol = np.sqrt(abs(var_diff / time_diff)) * 100
+              else:
+                  ivol = np.nan 
+              ivol_matrix.iloc[i, j] = ivol
+  ivol_matrix_float = ivol_matrix.astype(float)
+  plt.figure(figsize=(13, 11))
+  sns.heatmap(ivol_matrix_float, annot=True, fmt=".2f", cmap="viridis", cbar_kws={'label': 'Forward Volatility'})
+  plt.title('Forward Volatility Between Expiries')
+  plt.xlabel('Expiry Date')
+  plt.ylabel('Expiry Date')
+  plt.savefig(stockname+"_fv.png")
+  # plt.show()
